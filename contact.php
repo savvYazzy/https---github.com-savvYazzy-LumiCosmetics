e7010da1680d480aa763b7e2286d88b6f814335c
Yasmine Salim <ysalim@uwo.ca>
2023-03-09 10:41:40 -0500

    update

--- contact.php
+++ contact.php
@@ -6,14 +6,51 @@
 <h2 class="contact-heading">Contact Us</h2>
 <p class="contact-sub">If you have any existing concerns please reach out through our contact form below. We will try to get back to you as soon as possible</p>
 
-<br>
 <hr>
-<br>
 
-<form id="contact-form">
+<form id="contact-form" class="contact-form-box" action="mail.php" method="post">
+
+    <input name="name" type="text" placeholder="Name" required><br>
+ 
+    <input name="email" type="email" placeholder="Email" required><br>
+           
+    <input name="phone" type="tel" placeholder="Phone" required><br>
+       
+    <input name="subject" type="text" placeholder="Subject" required><br>
+               
+    <textarea name="message" placeholder="Please state your concern" required></textarea><br>
+
+     <button type="submit" class="">Submit Message</button><br>
+        <p class="form-messege"></p>
+   
+    </form>
+    
+                             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <form id="contact-form" class="default-form contact-form" action="mail.php" method="post">
   <div class="form-group">
     <label for="exampleFormControlInput1">Existing Username:</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="johnappleseed123">
+    <input type="text" name="username" class="form-control" id="exampleFormControlInput1" placeholder="johnappleseed123">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Existing Username:</label>
+    <input type="email" name="email" class="form-control" id="exampleFormControlInput1" placeholder="johnappleseed123">
   </div>
   <div class="form-group">
     <label for="exampleFormControlSelect1">Topic:</label>
@@ -27,21 +64,12 @@
     </select>
   </div>
   <div class="form-group">
-    <label for="exampleFormControlSelect2">Priority Rank</label>
-    <select multiple class="form-control" id="exampleFormControlSelect2">
-      <option>Low</option>
-      <option>Medium</option>
-      <option>High</option>
-      <option>Urgent</option>
-    </select>
-  </div>
-  <div class="form-group">
     <label for="exampleFormControlTextarea1">Please explain your inquiry in detail | We will respond to you within 1-3 business days.</label>
     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
   </div>
   <input type="submit" name="submit" value="Submit" class="btn btn-primary">
 
-</form>
+</form> -->
 
 
 
--- views/layout/body.php
+++ views/layout/body.php
@@ -27,19 +27,4 @@
 
 
 
-<!-- ------- PREVIOUS CODE // NOT RELEVANT -------- -->
-
-<!-- <main>
-    <div id="categories">
-<span id="cat_label1">Brushes
-<img src="../images/brushes.png" alt="category1" class="category" id="cat1"></span>
-<span id="cat_label2">Exfoliates
-<img src="../images/exfoliates.png" alt="category2" class="category" id="cat2"></span>
-<span id="cat_label3">Soap Bars
-<img src="../images/soapBars.png" alt="category3" class="category" id="cat3"></span>
-<span id="cat_label4">Hand Soaps
-<img src="../images/handSoaps.png" alt="category4" class="category" id="cat4"></span>
-
-    </div>
-</main> -->
 
