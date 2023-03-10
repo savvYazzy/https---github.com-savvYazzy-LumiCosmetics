6ab37aa608e94062c3634673a1de28ad534f90e4
Yasmine Salim <ysalim@uwo.ca>
2023-03-10 00:18:24 -0500

    completed

--- admin.php
+++ admin.php
@@ -1,7 +1,9 @@
 
 <?php require('views/layout/nav.php'); ?>
 <?php require('views/layout/header.php'); ?>
+
 <?php require('admin_login.php'); ?> 
+
 <?php require('views/layout/footer.php'); ?>
 
 
